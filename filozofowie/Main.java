6675b53002624d703764510658ff49adfe3c4e71
SosnowskiGrzegorz <74205700+SosnowskiGrzegorz@users.noreply.github.com>
2020-11-09 19:20:40 +0100

    Update Main.java

--- filozofowie/Main.java
+++ filozofowie/Main.java
@@ -1,21 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package filozofowie;
 
-/**
- *
- * @author Grzesiek
- */
-public class Main {
+import java.io.IOException;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+
+        int rodzaj;
+        do {
+            wyczyscKonsole();
+            System.out.println("Wybierz:");
+            System.out.println("1. Wykorzystanie Semaforów.");
+            System.out.println("2. Niesymetryczne sięganie po widelec.");
+            System.out.println("3. Wykorzystanie rzutu monetą.");
+            rodzaj = scanner.nextInt();
+        }while(rodzaj < 1 || rodzaj > 3);
+
+        int ilosc;
+        do{
+            wyczyscKonsole();
+            System.out.println("Ilość filozofów od 2 do 100:");
+            ilosc = scanner.nextInt();
+        }while(ilosc <2 || ilosc > 100);
+        
+        scanner.close();
+
+        Filozofowie filozof;
+        if (rodzaj == 1)
+            filozof = new FSem(ilosc);
+        else if (rodzaj == 2)
+            filozof = new FNiesym(ilosc);
+        else if (rodzaj == 3)
+            filozof = new FMoneta(ilosc);
+        else {
+            filozof = new FSem(2);
+            System.exit(1);
+        }
+
+        filozof.uruchomProblem();
     }
-    
+
+    private static void wyczyscKonsole()
+    {
+        try {
+            Runtime.getRuntime().exec("clear");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
